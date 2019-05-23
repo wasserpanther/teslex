8d5634e1b51b9a8594ca38036946a503e3e8baaf
Arno Kellermann <arno.kellermann@dcso.de>
2019-05-23 18:42:32 +0200

    added fuction specs

--- lib/teslex.ex
+++ lib/teslex.ex
@@ -14,6 +14,7 @@ defmodule Teslex do
   """
   use Tesla
 
+  @spec gettoken(any(), any()) :: Tesla.Env.t()
   def gettoken(email, pass) do
     clientId = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384"
     clientSecret = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3"
@@ -31,67 +32,80 @@ defmodule Teslex do
     response
   end
 
+  @spec getVehicle(binary() | Tesla.Client.t()) :: any()
   def getVehicle(client) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles")
     resp.body
   end
 
+  @spec getVehicle(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicle(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id)
     resp.body
   end
 
+  @spec getVehicleData(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleData(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/vehicle_data")
     resp.body
     # TODO: wake vehicle if unavailable
   end
 
+  @spec getVehicleChargeState(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleChargeState(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/charge_state")
     resp.body
   end
 
+  @spec getVehicleClimateState(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleClimateState(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/climate_state")
     resp.body
   end
 
+  @spec getVehicleDriveState(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleDriveState(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/drive_state")
     resp.body
   end
 
+  @spec getVehicleGUISettings(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleGUISettings(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/gui_settings")
     resp.body
   end
 
+  @spec getVehicleState(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleState(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/vehicle_state")
     resp.body
   end
 
+  @spec getVehicleConfig(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleConfig(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/data_request/vehicle_config")
     resp.body
   end
 
+  @spec getVehicleMobile(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleMobile(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/mobile_enabled")
     resp.body
   end
 
+  @spec getVehicleNearbyChargers(binary() | Tesla.Client.t(), binary()) :: any()
   def getVehicleNearbyChargers(client, id) do
     {:ok, resp} = Tesla.get(client, "/api/1/vehicles/"<> id <> "/nearby_charging_sites")
     resp.body
   end
 
+  @spec wakeVehicle(binary() | Tesla.Client.t(), binary()) :: any()
   def wakeVehicle(client, id) do
     {:ok, resp} = Tesla.post(client, "/api/1/vehicles/"<> id <> "/wake_up", "good morning")
     resp.body
   end
 
+  @spec scheduleUpdate(binary() | Tesla.Client.t(), binary()) :: any()
   def scheduleUpdate(client, id) do
     {:ok, resp} = Tesla.post(client, "/api/1/vehicles/"<> id <> "/command/schedule_software_update", "yay new stuff \o/")
     resp.body
@@ -102,6 +116,7 @@ defmodule Teslex do
     resp.body
   end
 
+  @spec navigationRequest(binary() | Tesla.Client.t(), binary(), any()) :: any()
   def navigationRequest(client, id, dest) do
     reqBody = %{
         "type" => "share_ext_content_raw",
@@ -115,6 +130,7 @@ defmodule Teslex do
     resp.body
   end
 
+  @spec client(binary()) :: Tesla.Client.t()
   def client(token) do
     middleware = [
       {Tesla.Middleware.BaseUrl, "https://owner-api.teslamotors.com"},
